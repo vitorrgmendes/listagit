6a085e03075a6df41b73c0160b9ffd7b2fdbaff7
Vitor Mendes <vitormendes20@gmail.com>
2023-10-03 13:11:32 -0300

    Criação do Projeto para Atividade

---
+++ src/main/java/com/projetogit/usuariogit/UsuariogitApplication.java
@@ -0,0 +1,13 @@
+package com.projetogit.usuariogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuariogitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuariogitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetogit/usuariogit/UsuariogitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetogit.usuariogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariogitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
